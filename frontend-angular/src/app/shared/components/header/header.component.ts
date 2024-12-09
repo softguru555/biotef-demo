32a7db996aa7e8030465e55e5ab995c1e12a171b
softguru555 <softguru555@gmail.com>
2024-12-08 23:46:13 -0600

    dashboard PAGE

--- frontend-angular/src/app/shared/components/header/header.component.ts
+++ frontend-angular/src/app/shared/components/header/header.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+
 @Component({
   selector: 'app-header',
   imports: [
