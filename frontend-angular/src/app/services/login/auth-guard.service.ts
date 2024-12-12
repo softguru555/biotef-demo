40df8b6e27c49ae68a4d8cce1c6f19937cc308af
softguru555 <softguru555@gmail.com>
2024-12-12 01:54:02 -0600

    add localstorageService to auth guard

--- frontend-angular/src/app/services/login/auth-guard.service.ts
+++ frontend-angular/src/app/services/login/auth-guard.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { LoginService } from './login.service';
+import { LocalStorageService } from '../storage/local-storage.service';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuardService implements CanActivate {
-  constructor(private loginService: LoginService) {}
+  constructor(
+    private loginService: LoginService,
+    private localStorage: LocalStorageService
+  ) {}
 
   canActivate(): boolean {
-    if (localStorage.getItem('userInfo')) return true;
+    if (this.localStorage.getItem('userInfo')) return true;
 
     this.loginService.logout();
     return false;
